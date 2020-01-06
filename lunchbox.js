04d6b155f34f790e485392acce92ee3597b2aea6
Megan Venetianer <megan.venetianer@gmail.com>
2020-01-05 17:18:40 -0700

    Passed lunchbox tests

--- lunchbox.js
+++ lunchbox.js
@@ -1,5 +1,20 @@
 class LunchBox {
+  constructor(box) {
+    this.owner = box.owner;
+    this.material = box.madeOf;
+    this.shape = box.shape;
+    this.color = box.color;
+    this.handle = true;
+    this.snacks = [];
+  }
+    acquireSnack(snack) {
+      this.snacks.push(snack);
+      snack.isInLunchBox = true;
+    }
+    findHealthySnacks() {
+      return this.snacks.filter(snack => snack.healthy === true);
+      }
+  }
 
-}
 
 module.exports = LunchBox;
--- snack.js
+++ snack.js
@@ -1,5 +1,22 @@
 class Snack {
-
+  constructor(type) {
+    this.deliciousLevel = "extra";
+    this.type = type;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+    if(type.includes('Fruit')) {
+      this.healthy = true;
+    } else {
+      this.healthy = false;
+    }
+  }
+  getEaten() {
+    this.amount -=10;
+    if(this.amount <= 20) {
+    this.cuttingItClose = true;
+    }
+  }
 }
 
 module.exports = Snack;
